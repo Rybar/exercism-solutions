ce6e94dd01b7e89465edee0fe8aeed160a22a0c0
Ryan Malm <ryan.malm@gmail.com>
2015-05-29 02:45:32 +0000

    bob exercism complete

--- exercism/javascript/bob/bob.js
+++ exercism/javascript/bob/bob.js
@@ -3,12 +3,29 @@
 // convenience to get you started writing code faster.
 //
 
+//Can the logic be restructured so the '?' check doesn't need repeated? 
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+
+if(input == input.toUpperCase()){ //are we yelling?
+    
+    if(input.replace( /[A-Z]/g, "") == input) { //does input contain only numbers &|| punctuation?
+    
+        if(input.slice(-1) == "?") { // is input a silly number question?  
+            return "Sure.";
+        }
+        else return "Whatever."; //must be just numbers &|| other chars and not a question.
+    }
+    
+    else return "Whoa, chill out!"
+}
+
+if(input.slice(-1) == "?") { //only if the last character is ? do we consider input a question
+    return "Sure.";
+}
+
+else return "Whatever.";
 };
 
 module.exports = Bob;
