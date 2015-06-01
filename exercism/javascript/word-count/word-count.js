f437a02df2fccc9583efb7d131a07f0add13636e
rybar <ryan.malm@gmail.com>
2015-06-01 05:40:05 +0000

    Word Count complete

--- exercism/javascript/word-count/word-count.js
+++ exercism/javascript/word-count/word-count.js
@@ -1,15 +1,12 @@
 var words = function(input) {
- var arr = input.replace(/\s+/g,' ').replace(/\n+/g, ' ').split(' ');
- console.log(arr);
- var counts = {};
- var i = 0;
-  
-    for(i = 0; i <= arr.length-1; i++) {
-        var word = arr[i];
-        counts["" + word] = counts["" + word] || 0;
-        counts["" + word]++;
-    }
-    
+
+var counts = Object.create(null);
+var arr = input.replace(/\s+/g,' ').replace(/\n+/g, ' ').split(' ').reduce(function(acc, curr) {
+  acc[curr] = acc[curr] || 0;
+  acc[curr]++;
+  return counts;
+}, counts);
+
 return counts;
 
 };
