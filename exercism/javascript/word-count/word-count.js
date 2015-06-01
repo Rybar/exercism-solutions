1112672fff0a567537a1c57a47106e8f560a00b9
rybar <ryan.malm@gmail.com>
2015-06-01 04:19:52 +0000

    word-count complete save for one test involving object prototype keywords

--- exercism/javascript/word-count/word-count.js
+++ exercism/javascript/word-count/word-count.js
@@ -1,7 +1,17 @@
-var WordCount = function() {};
-
-WordCount.prototype.words = function(input) {
- 
- 
+var words = function(input) {
+ var arr = input.replace(/\s+/g,' ').replace(/\n+/g, ' ').split(' ');
+ console.log(arr);
+ var counts = {};
+ var i = 0;
+  
+    for(i = 0; i <= arr.length-1; i++) {
+        var word = arr[i];
+        counts["" + word] = counts["" + word] || 0;
+        counts["" + word]++;
+    }
     
-}
+return counts;
+
+};
+
+module.exports = words;
