246cbcf5f4b0c2687a3e86b642970c3c2ef1c80c
rybar <ryan.malm@gmail.com>
2015-06-03 04:54:35 +0000

    begun working on hamming length problem

---
+++ exercism/javascript/hamming/hamming.js
@@ -0,0 +1,12 @@
+var hamming = function() {};
+
+hamming.compute = function(inputA, inputB) {
+ if(inputA.length != inputB.length) {
+     throw 'DNA strands must be of equal length.';
+ }
+
+return 0;
+
+};
+
+module.exports = hamming;
