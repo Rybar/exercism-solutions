0585bb8d3210b8aee690fb2040cb521411fa5657
rybar <ryan.malm@gmail.com>
2015-06-04 03:25:09 +0000

    Hamming distance solved

--- exercism/javascript/hamming/hamming.js
+++ exercism/javascript/hamming/hamming.js
@@ -4,8 +4,18 @@ hamming.compute = function(inputA, inputB) {
  if(inputA.length != inputB.length) {
      throw 'DNA strands must be of equal length.';
  }
+ else {
+     var i = 0,
+         distance = 0;
+     
+     for(i = 0; i <= inputA.length; i++){
+         if(inputA[i] !== inputB[i]) {
+             distance++;
+         }
+     }
+ }
 
-return 0;
+return distance;
 
 };
 
